f1f28fdf4d30ae67b73b19c95cba9053615b0b45
Naman12769 <namanjainjain341@gmail.com>
2025-04-15 16:47:49 +0530

    fixing the flake8

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 def add(a, b):
-  return a + b
+    return a + b
 
 
-def sub(a, b): 
-  return a - b
+def sub(a, b):
+    return a - b
 
-result1=add(4, 5)
-result2=sub(10, 6)
+
+result1 = add(4, 5)
+result2 = sub(10, 6)
 
 print(result1, result2)
-
